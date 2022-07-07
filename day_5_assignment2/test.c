44f12a709d5ea1c3b74d00a69c38b0eaaff4b7a2
shashank <shashank12237@gmail.com>
2022-07-07 18:45:35 +0530

    yaswant sir book

---
+++ day_5_assignment2/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//* questions from yashwant kenkar sir book
+int main()
+{
+// float a = 4;
+// int i = 2;
+// printf ("%f   %d \n", i/a, i / a ) ;
+// printf ("%d  %f\n",i/a,i / a ) ;
+
+// int p, q;
+// scanf ("Enter values of p and q %d %d", &p, &q);
+// printf ("p = %d q = %d", p,q);
+int p, q;
+printf ("Enter values of p and q");
+scanf ("%d\n\n%d", &p,&q);
+printf ("p = %d q = %d", p, q ) ;
+
+    return 0;
+}
