5d8dafe70522791d18608aa0b2b08eed24ae089d
shashank <shashank12237@gmail.com>
2022-07-12 16:55:15 +0530

    comma_operator_for_loop

---
+++ doubts/comma_for_loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ int i=0,j=0,k=0;
+ for(i=0,j=-3;i<3,j<3;i++,j++)
+ {k=i+j;
+ }
+ printf("%d",i+j);
+ printf("\n %d",j);
+ printf("\n %d",i);
+ printf("\n %d",k);
+
+    return 0;
+}
