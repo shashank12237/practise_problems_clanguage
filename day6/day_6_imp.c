3029c597dc8a1f3c68d8e852d41825a25f7730f8
shashank <shashank12237@gmail.com>
2022-07-13 02:50:20 +0530

    imp

--- day6/day_6_imp.c
+++ day6/day_6_imp.c
@@ -3,18 +3,25 @@
 int main( )
 {
     
-    //int a; 
-    //a = printf("hello world");
-    //printf("%d",a); // 5  
+    int a; 
+    a = printf("hello world");
+    //* execute printf and gives Hello world as o/p
+    printf("%d",a); 
+    //* since a is a int type variable 
+    //* so it will have only integer value
+    //* it means it will count number of words
+    //* in string h e l l o space w o r l  d 
+    //*           1 2 3 4 5 6     7 8 9 10 11
+    //* o/p==>11
 
     // int x; 
     // int num = 10; 
     // x = printf(" %10d ",num); 
     // printf(" %d",x); 
 
-    int a; 
-    a = printf("\b\t\r\nhello");
-    printf("%d",a); 
+    // int b; 
+    // b = printf("\b\t\r\nhello");
+    // printf("%d",b); 
 
 
     return 0; 
--- day6/tempCodeRunnerFile.c
+++ day6/tempCodeRunnerFile.c
@@ -1,10 +1,3 @@
-int ans;
-    int a = 0; 
-    int b = 1;
-    int c = 0; 
-
-    ans = a++ && b++ || c++;
-    //    (a++ && b++) || c++
-    //    (F++ && XX) || 
-    //         F  ||  F++
-    //            F    
+   // int b; 
+    // b = printf("\b\t\r\nhello");
+    // printf("%d",b); 
