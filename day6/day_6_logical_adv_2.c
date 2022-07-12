c8eabe70c3a09c519b87a64477e44bdee9a52b64
shashank <shashank12237@gmail.com>
2022-07-13 01:17:01 +0530

    day 6 logicl oprtr and oprtn

---
+++ day6/day_6_logical_adv_2.c
@@ -0,0 +1,48 @@
+#include<stdio.h> 
+
+int main( )
+{
+    // int ans;
+    // int a = 0; 
+    // int b = 0; 
+    
+    // ans = a++ && b++; 
+    // //   F++ && XXXX
+    //  //   F  
+
+   // printf("a = %d b=%d ans = %d",a,b,ans);   
+   
+    // int ans;
+    // int a = 1; 
+    // int b = 0; 
+    
+    // ans = a++ && b++; 
+    // //    T++ && F++
+    //  //    F
+
+    //printf("a = %d b=%d ans = %d",a,b,ans);   
+   
+
+    // int ans;
+    // int a = 0; 
+    // int b = 0; 
+    
+    // ans = ++a && b++;
+    // //     T  && F++ 
+    // //        F     
+    
+    // printf("a = %d b=%d ans = %d",a,b,ans);   
+
+
+    // int ans;
+    // int a = 0; 
+    // int b = 0; 
+    
+    // ans = ++a && ++b;
+    // //      T && T
+    // //         T 
+
+    // printf("a = %d b=%d ans = %d",a,b,ans);   
+    return 0;
+ 
+}
