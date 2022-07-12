07b2519ed87793b0a395bc17350995bdf5f9375b
shashank <shashank12237@gmail.com>
2022-07-13 02:14:36 +0530

    comma oprtr in for loop

---
+++ day6/day_6_imp.c
@@ -0,0 +1,22 @@
+#include<stdio.h> 
+
+int main( )
+{
+    
+    //int a; 
+    //a = printf("hello world");
+    //printf("%d",a); // 5  
+
+    // int x; 
+    // int num = 10; 
+    // x = printf(" %10d ",num); 
+    // printf(" %d",x); 
+
+    int a; 
+    a = printf("\b\t\r\nhello");
+    printf("%d",a); 
+
+
+    return 0; 
+}
+
